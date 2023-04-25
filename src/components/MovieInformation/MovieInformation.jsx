b0b29b00f57428890f8eedc81078b58206fef2d8
Tahzibi <m.r.tahzibi@gmail.com>
2023-04-25 20:20:18 +0430

    Add assets and file structure

---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const MovieInformation = () => {
+  console.log("Movie Info");
+  return <div>MovieInformation</div>;
+};
+
+export default MovieInformation;
---
+++ src/styles.js
@@ -0,0 +1,10 @@
+import { createStyles, makeStyles } from "@mui/styles";
+
+export default makeStyles((theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      height: "100%",
+    },
+  })
+);
